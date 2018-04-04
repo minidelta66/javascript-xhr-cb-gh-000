d715ef37a0d2644eda965dff67ee948c9068c150
Andrzej Nicpoń <andrzejmd66@gmail.com>
2018-04-04 23:04:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function showRepositories(event, data) {
   var repos = JSON.parse(this.responseText);
   console.log(repos);
-  const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`;
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
   /* console.log(this.responseText);
   let repoList = "<ul>";
   for(var i=0; i < this.responseText.length; i++) {
     repoList += '<li>' + this.responseText[i]['name'] + '</li>'
   }
    repoList += '</ul>'; */
-  document.getElementById('repositories').innerHTML = repoList;
+  document.getElementById('repositories').innerHTML = repoList; // list of repos
 }
 
 
@@ -16,5 +16,13 @@ function getRepositories() {
   const req = new XMLHttpRequest();
   req.addEventListener('load', showRepositories)
   req.open('GET', 'https://api.github.com/users/minidelta66/repos');
+  req.send();
+}
+
+function getCommits(el) {
+  const name = el.dataset.repo;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showCommits);
+  req.open('GET', 'https://api.github.com/repos/minidelta66/' + name + '/commits');
   req.send();
 }

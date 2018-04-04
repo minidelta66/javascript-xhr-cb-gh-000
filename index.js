fc2fd983f111e9acd6c1e0942acdd5210a00a7b1
Andrzej Nicpoń <andrzejmd66@gmail.com>
2018-04-04 23:06:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,3 +26,9 @@ function getCommits(el) {
   req.open('GET', 'https://api.github.com/repos/minidelta66/' + name + '/commits');
   req.send();
 }
+
+function showCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+  document.getElementById("commits").innerHTML = commitsList;
+}

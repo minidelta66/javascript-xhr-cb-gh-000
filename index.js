e6fb42957fa2e1ff8b5fa89af41ab8f2ad895e78
Andrzej Nicpoń <andrzejmd66@gmail.com>
2018-04-04 22:46:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+function showRepositories(event, data) {
+  console.log(this.responseText);
+}
+
+
 function getRepositories() {
   const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositories)
   req.open('GET', 'https://api.github.com/users/minidelta66/repos');
   req.send();
 }
